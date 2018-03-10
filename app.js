0ad1094baab723d74ac9a7dcfc7d8421e5d240dd
terrygonguet <terry@gonguet.com>
2018-03-10 02:45:30 +0000

    Base Rest API

--- app.js
+++ app.js
@@ -1,18 +1,55 @@
 const restify = require('restify');
-const mongodb = require('mongodb').MongoClient;
+const errors = require('restify-errors');
+const db = require('./lib/db');
+const _ = require('lodash');
 
 const server = restify.createServer();
-const url = require('./lib/config/local').mongodb.url;
+server.pre(restify.plugins.pre.sanitizePath());
+server.pre(restify.plugins.pre.context());
+
+server.use(restify.plugins.queryParser({ mapParams: false }));
+server.use(restify.plugins.bodyParser());
+server.use(restify.plugins.gzipResponse());
+
+server.get('/comments', function (req, res, next) {
+  if (!req.query.url) {
+    return next(new errors.MissingParameterError('Missing query parameter : url'));
+  }
+  db.getComments({ url:req.query.url })
+    .then(comments => {
+      res.send(comments);
+      next();
+    })
+    .catch(e => {
+      console.log(e);
+      next(new errors.InternalServerError(e));
+    });
+});
+
+server.post('/comments', function (req, res, next) {
+  const props = [ "text", "url", "x", "y" ];
+  if (_.difference(props, _.keys(req.body)).length) {
+    return next(new errors.InvalidArgumentError('Missing properties in request body'));
+  }
+  db.postComment(_.pick(req.body, props))
+    .then(result => {
+      res.status(201);
+      res.end();
+      next();
+    })
+    .catch(e => {
+      console.log(e);
+      next(new errors.InternalServerError(e));
+    });
+});
+
+server.del('/comments', function (req, res, next) {
+  db.deleteComments(req.query);
+  res.status(200);
+  res.end();
+  next();
+});
 
 server.listen(process.env.PORT || 8080, async function () {
   console.log("Server started on " + server.url);
-  var client = await mongodb.connect(url);
-  var db = client.db('wwc');
-  var col = db.collection('comments');
-  await col.insertOne({ message:"test" });
-  await new Promise(function(resolve, reject) {
-    col.find().forEach(console.log, resolve);
-  });
-  await col.deleteMany({});
-  client.close();
 });
